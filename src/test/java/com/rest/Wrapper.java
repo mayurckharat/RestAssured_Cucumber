5246ef51884891a74927a789b974ee491c5774dc
mayurckharat <mayurc.kharat@gmail.com>
2023-10-31 14:04:06 +0530

    for code change

--- src/test/java/com/rest/Wrapper.java
+++ src/test/java/com/rest/Wrapper.java
@@ -15,6 +15,7 @@ public class Wrapper {
             file = new FileInputStream("./src/test/resources/config.properties");
         }
         catch (FileNotFoundException e) {
+        	//just for code change
             e.printStackTrace();
         }
     }
